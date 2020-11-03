711dd80b2223b23d239e3df018c1e64bff656c2a
David Boland <dboland77@gmail.com>
2020-11-03 16:54:24 +0000

    Added builder controls to add and remove ingredients and update the price

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
   .map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_,i)=>{
       return <BurgerIngredient key ={igKey+i} type = {igKey} />
     });
-  });
+  })
+  .reduce((arr,el)=> {
+    return arr.concat(el)
+  },[]);
+ 
+  if (transformedIngredients.length === 0){
+    transformedIngredients =<p> Please add some ingredients!</p>
+  }
+  
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top"/>
