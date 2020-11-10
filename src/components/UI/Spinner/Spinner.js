79caec7166a6ec966dbaf47d5dc36bc577ec1279
David Boland <dboland77@gmail.com>
2020-11-10 10:36:07 +0000

    Added spinner for long loads and axios post update to firebase

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./Spinner.module.css";
+
+const Spinner = () => {
+  return <div className={Styles.Loader}>Loading...</div>;
+};
+
+export default Spinner;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -23,6 +24,7 @@ class BurgerBuilder extends Component {
     totalPrice: 4,
     purchasable: false,
     purchasing: false,
+    loading: false,
   };
 
   updatePurchaseState = (ingredients) => {
@@ -82,23 +84,26 @@ class BurgerBuilder extends Component {
   // than the class)
   purchaseContinueHandler = () => {
     //alert("You continue!");
+    this.setState({ loading: true });
+
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
       customer: {
-        name: 'Dave Boland',
+        name: "Dave Boland",
         address: {
-          street: 'My street',
-          postcode: 'PO12345',
-          country: 'United Kingdom'
+          street: "My street",
+          postcode: "PO12345",
+          country: "United Kingdom",
         },
-        email: 'test@test.com'
+        email: "test@test.com",
       },
-      deliveryMethod: 'fastest'
-    }
-    axios.post("/orders.json",order)
-    .then(response=> console.log(response))
-    .catch(error => console.log(error))
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => this.setState({ loading: false, purchasing: false }))
+      .catch((error) => this.setState({ loading: false, purchasing: false }));
   };
 
   render() {
@@ -108,18 +113,26 @@ class BurgerBuilder extends Component {
     for (let key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
+    let orderSummary = (
+      <OrderSummary
+        ingredients={this.state.ingredients}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler}
+        price={this.state.totalPrice}
+      />
+    );
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
+
     return (
       <Fragment>
         <Modal
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}
-            price={this.state.totalPrice}
-          />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
